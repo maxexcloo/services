751f9b65f494dd40838eb0ff7983f837f0eca338
Max Schaefer <max@excloo.com>
2024-06-06 23:45:33 +1000

    simplify

--- deploy.py
+++ deploy.py
@@ -1,25 +1,26 @@
+from requests import Session
 import json
 import os
-import requests
 
 app = os.getenv("DIR")
 apps = json.loads(os.getenv("APPS"))
-portainer_headers = {"Content-Type": "application/json", "X-API-Key": os.getenv("PORTAINER_API_TOKEN")}
+portainer_api_token = os.getenv("PORTAINER_API_TOKEN")
 portainer_url = f"{os.getenv("PORTAINER_URL")}/api"
 
-endpoints = []
-stacks = []
+r = requests.Session()
+r.headers.update({"Content-Type": "application/json", "X-API-Key": portainer_api_token})
 
-response = requests.get(f"{portainer_url}/endpoints", headers=portainer_headers)
+endpoints = []
+response = r.get(f"{portainer_url}/endpoints")
 if response.status_code == 200:
-    for endpoint in response.json():
-        endpoints.append({"id": endpoint["Id"], "name": endpoint["Name"]})
+    endpoints = response.json()
 else:
     print("Failed to retrieve endpoints:", response.status_code)
 
 print(endpoints)
 
-response = requests.get(f"{portainer_url}/stacks", headers=portainer_headers)
+stacks = []
+response = r.get(f"{portainer_url}/stacks")
 if response.status_code == 200:
     stacks = response.json()
 else:
