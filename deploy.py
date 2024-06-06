bd249349668fae2676168ef2a4ecc666b3e127c5
Max Schaefer <max@excloo.com>
2024-06-06 23:30:51 +1000

    push stacks

--- deploy.py
+++ deploy.py
@@ -2,18 +2,27 @@ import json
 import os
 import requests
 
-dir = os.getenv("APP_DIR")
+app = os.getenv("DIR")
+apps = json.loads(os.getenv("APPS"))
 portainer_headers = {"Content-Type": "application/json", "X-API-Key": os.getenv("PORTAINER_API_TOKEN")}
 portainer_url = f"{os.getenv("PORTAINER_URL")}/api"
-stacks = json.loads(os.getenv("STACKS"))
 
 endpoints = []
+stacks = []
+
 response = requests.get(f"{portainer_url}/endpoints", headers=portainer_headers)
 if response.status_code == 200:
-    data = response.json()
     for endpoint in response.json():
         endpoints.append({"id": endpoint["Id"], "name": endpoint["Name"]})
 else:
     print("Failed to retrieve endpoints:", response.status_code)
 
 print(endpoints)
+
+response = requests.get(f"{portainer_url}/stacks", headers=portainer_headers)
+if response.status_code == 200:
+    stacks = response.json()
+else:
+    print("Failed to retrieve endpoints:", response.status_code)
+
+print(stacks)
