07975add124832dffd6af9088a2e52a6b2f6b89b
Max Schaefer <max@excloo.com>
2024-06-07 15:10:25 +1000

    refactor

--- deploy.py
+++ deploy.py
@@ -1,6 +1,8 @@
 import json
 import os
 import requests
+import sys
+
 
 class APIClient:
     def __init__(self, base_url, headers):
@@ -20,6 +22,7 @@ class APIClient:
     def put(self, endpoint, **kwargs):
         return self.session.put(f"{self.base_url}/{endpoint}", **kwargs)
 
+
 def read_env_var(var_name):
     try:
         value = os.getenv(var_name)
@@ -29,14 +32,23 @@ def read_env_var(var_name):
     except Exception as error:
         raise SystemExit(f"An error occurred while reading the environment variable '{var_name}': {error}")
 
-def read_file(file_path):
+
+def read_file(file_path, readlines=False):
     try:
         with open(file_path, "r") as file:
-            return file.read()
+            if readlines:
+                return file.readlines()
+            elif file_path.lower().endswith(".json"):
+                return json.load(file)
+            else:
+                return file.read()
     except FileNotFoundError:
         raise SystemExit(f"Error: The file {file_path} was not found.")
-    except Exception as error:
+    except json.JSONDecodeError:
+        raise SystemExit(f"Error: The file {file_path} contains invalid JSON.")
+    except Exception as e:
         raise SystemExit(f"An unexpected error occurred: {error}")
+
 
 def read_json(value):
     try:
@@ -46,64 +58,96 @@ def read_json(value):
     except Exception as error:
         raise SystemExit(f"An unexpected error occurred: {error}")
 
+
 if __name__ == "__main__":
-    item = read_env_var("DIR")
-    item_endpoint_names = read_json(read_file(f"{item}/endpoints.json"))
-    portainer = APIClient(f"{read_env_var("PORTAINER_URL")}/api", {"Content-Type": "application/json", "X-API-Key": read_env_var("PORTAINER_API_TOKEN")})
+    item = sys.argv[1]
+    item_docker_file = read_file(f"{item}/docker-compose.yaml")
+    item_endpoint_names = read_file(f"{item}/endpoints.json")
+    item_envs = read_file(f"{item}/stack.env", readlines=True)
+
+    defaults = read_json(read_env_var("DEFAULTS"))
     websites = read_json(read_env_var("WEBSITES"))
 
+    portainer = APIClient(
+        f"{read_env_var('PORTAINER_URL')}/api",
+        {
+            "Content-Type": "application/json",
+            "X-API-Key": read_env_var("PORTAINER_API_TOKEN"),
+        },
+    )
+
     portainer_endpoints = []
-    response = portainer.get(f"{portainer_url}/endpoints")
+    response = portainer.get(f"endpoints")
     if response.status_code == 200:
         portainer_endpoints = response.json()
     else:
         raise SystemExit(f"Failed to retrieve Portainer endpoints: {response.status_code}")
 
     portainer_stacks = []
-    response = portainer.get(f"{portainer_url}/stacks")
+    response = portainer.get(f"stacks")
     if response.status_code == 200:
         portainer_stacks = response.json()
     else:
         raise SystemExit(f"Failed to retrieve Portainer stacks: {response.status_code}")
 
     item_endpoints = [
-        {"id": portainer_endpoint["Id"], "name": portainer_endpoint["Name"]} for portainer_endpoint in portainer_endpoints
+        {"id": portainer_endpoint["Id"], "name": portainer_endpoint["Name"]}
+        for portainer_endpoint in portainer_endpoints
         if "all" in item_endpoint_names or portainer_endpoint["Name"] in item_endpoint_names
     ]
 
     item_env = []
-    for line in read_file(f"{stack}/.env"):
-        split = line.rstrip().split("=")
-        item_env.append({split[0]: split[1]})
+    for stack_env in item_envs:
+        stack_env_split = stack_env.rstrip().split("=")
+        item_env.append({"name": stack_env_split[0], "value": stack_env_split[1]})
+    item_env.append({"name": "DOMAIN_EXTERNAL", "value": defaults["domain_external"]})
+    item_env.append({"name": "DOMAIN_INTERNAL", "value": defaults["domain_internal"]})
+    item_env.append({"name": "EMAIL", "value": defaults["email"]})
+    item_env.append({"name": "TIMEZONE", "value": defaults["timezone"]})
 
+    print(f"Processing {item}:")
     for item_endpoint in item_endpoints:
-        if any(portainer_stack["EndpointId"] == item_endpoint.id and portainer_stack["Name"] == item for portainer_stack in portainer_stacks):
-            print(f"'{item}' exists on endpoint '{item_endpoint.name}', redeploying...")
+        item_env_endpoint = item_env.copy()
+        for website in websites:
+            if website["host"] == item_endpoint['name'] and website["type"] == item:
+                for key, value in website.items():
+                    item_env_endpoint.append({"name": key.upper(), "value": value})
+
+        print(item_env_endpoint)
+
+        if any(portainer_stack["EndpointId"] == item_endpoint["id"] and portainer_stack["Name"] == item for portainer_stack in portainer_stacks):
+            print(f"Exists on endpoint '{item_endpoint['name']}', redeploying...")
             data = {
-                "env": item_env,
+                "env": item_env_endpoint,
                 "prune": True,
                 "pullImage": True,
-                "stackFileContent": read_file(f"{item}/docker-compose.yaml")
+                "stackFileContent": item_docker_file,
             }
+            id = next(portainer_stack["Id"] for portainer_stack in portainer_stacks if portainer_stack["EndpointId"] == item_endpoint["id"] and portainer_stack["Name"] == item)
             params = {
-                "endpointId": item_endpoint.id,
-                "id": next(portainer_stack["Id"] for portainer_stack in portainer_stacks if portainer_stack["EndpointId"] == item_endpoint.id and portainer_stack["Name"] == item)
+                "endpointId": item_endpoint["id"],
             }
-            response = portainer.put(f"{portainer_url}/stacks", data=data, params=params)
+            response = portainer.put(f"stacks/{id}", json=data, params=params)
             if response.status_code == 200:
-                print(f"Successfully redeployed existing Portainer stack '{item}' to endpoint '{item_endpoint.name}'.")
+                print(f"Successfully redeployed existing Portainer stack '{item}' to endpoint '{item_endpoint['name']}'.")
             else:
-                print(f"Failed to redeploy existing Portainer stack '{item}' to endpoint '{item_endpoint.name}':", response.status_code)
+                print(
+                    f"Failed to redeploy existing Portainer stack '{item}' to endpoint '{item_endpoint['name']}':",
+                    response.status_code,
+                )
         else:
-            print(f"'{item}' does not exist on endpoint '{item_endpoint.name}', deploying...")
+            print(f"Does not exist on endpoint '{item_endpoint.name}', deploying...")
             data = {
-                "env": stack_env,
+                "env": item_env_endpoint,
                 "fromAppTemplate": False,
-                "name": stack,
-                "stackFileContent": read_file(f"{stack}/docker-compose.yaml")
+                "name": item,
+                "stackFileContent": item_docker_file,
             }
-            response = portainer.post(f"{portainer_url}/stacks/create/standalone/string", data=data)
+            response = portainer.post(f"stacks/create/standalone/string", json=data)
             if response.status_code == 200:
-                print(f"Successfully deployed new Portainer stack '{item}' to endpoint '{item_endpoint.name}'.")
+                print(f"Successfully deployed new Portainer stack '{item}' to endpoint '{item_endpoint['name']}'.")
             else:
-                print(f"Failed to deploy new Portainer stack '{item}' to endpoint '{item_endpoint.name}':", response.status_code)
+                print(
+                    f"Failed to deploy new Portainer stack '{item}' to endpoint '{item_endpoint['name']}':",
+                    response.status_code,
+                )
