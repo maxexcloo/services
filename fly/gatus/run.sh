fbd8bf05dd8daca15c397a072b25dfb5476771fd
Max Schaefer <max@excloo.com>
2024-09-06 14:46:09 +1000

    update deploy

--- fly/gatus/run.sh
+++ fly/gatus/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 tailscaled --socket /var/run/tailscale/tailscaled.sock --state /var/lib/tailscale/tailscaled.state &
-tailscale up --authkey ${TAILSCALE_TAILNET_KEY} --hostname ${NAME}
+tailscale up --authkey ${TAILSCALE_TAILNET_KEY} --hostname ${FLY_APP_NAME}
 ./gatus
