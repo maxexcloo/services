de1ee3b87e982aeab21af20d8df2802576b1e09b
Max Schaefer <max@excloo.com>
2025-04-22 00:17:48 +1000

    fix trigger

--- fly.tf
+++ fly.tf
@@ -57,6 +57,10 @@ resource "graphql_mutation" "fly_app_ip" {
     ignore_changes = [
       mutation_variables
     ]
+
+    replace_triggered_by = [
+      restapi_object.fly_app_service[each.key].api_data.id
+    ]
   }
 
   mutation_variables = {
