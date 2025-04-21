26b44197e1eb3177953a67f192065d6df37ed005
Max Schaefer <max@excloo.com>
2025-04-21 20:15:24 +1000

    add ignore_all_server_changes

--- resend.tf
+++ resend.tf
@@ -4,11 +4,12 @@ resource "restapi_object" "resend_api_key_service" {
     if service.enable_resend
   }
 
-  data         = jsonencode({ name = each.key })
-  id_attribute = "id"
-  path         = "/api-keys"
-  provider     = restapi.resend
-  read_path    = "/api-keys"
+  data                      = jsonencode({ name = each.key })
+  id_attribute              = "id"
+  ignore_all_server_changes = true
+  path                      = "/api-keys"
+  provider                  = restapi.resend
+  read_path                 = "/api-keys"
 
   read_search = {
     query_string = ""
